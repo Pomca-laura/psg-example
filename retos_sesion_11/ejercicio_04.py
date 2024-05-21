9edbb40e8af463542ca72c1f7d244cbc3cae3f85
Pomca-laura <pomca.laura@gmail.com>
2024-05-21 17:15:09 -0500

    Ejercicios sesión 11

---
+++ retos_sesion_11/ejercicio_04.py
@@ -0,0 +1,11 @@
+"""Gestión de hábitats en peligro: Crea un diccionario que asocie especies animales en peligro de extinción con información sobre sus hábitats amenazados, lo que permite priorizar la protección de áreas críticas para la supervivencia de estas especies
+habitats = {"polo norte" : {"especies": {"oso polar", "morsa", "ballena"}}, "amazonas" : {"especies": {"tigre", "mono", "guacamayo"}}}
+Añade al diccionario de habitats 2 habitats más usando update()
+Existe en el diccionario de habitats el habitat 'amazonas'?
+Añade al diccionario de amazonas la especie 'anaconda'
+Eres NOE y tienes que guardar dos animales de cada especie en un arca, crea un diccionario con las especies
+arca = {"perro" : 2, "gato" : 2, "tigre" : 2, "mono" : 2, "unicornio" : 0, "jirafa" : 1}
+Añade al diccionario de habitats 2 habitats más usando update()
+Existe en el diccionario de habitats el habitat 'amazonas'?
+Añade al diccionario de amazonas la especie 'anaconda'
+"""
