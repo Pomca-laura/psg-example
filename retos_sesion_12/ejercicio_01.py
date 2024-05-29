0e4822532438ce989340c980b1bbacc5c6d2334b
Pomca-laura <pomca.laura@gmail.com>
2024-05-29 08:55:50 -0500

    Ejercios sesión 12

---
+++ retos_sesion_12/ejercicio_01.py
@@ -0,0 +1,2 @@
+# Crear un script que pida un número entero y verifique si es par o impar usando operador ternario
+
---
+++ retos_sesion_12/ejercicio_03.py
@@ -0,0 +1,15 @@
+"""El usuario Jhon Doe esta en una red social sus amigos son:
+{Alice, Bob, Charlie, David, Eve}
+La usuaria Jess Doe tiene los siguientes amigos
+
+{Alice, Bob,  Frank, Grace}
+¿Tienen Jhon y Jess amigos en común?, ¿Cuáles son?
+
+Una tienda ofrece descuentos a sus clientes, si el cliente es mayor de edad y tiene una compra mayor a 1000, 
+se le aplica un descuento del 10%, si es menor de edad y tiene una compra mayor a 500 se le aplica 
+un descuento del 5%, si no cumple ninguna condición se le aplica un descuento del 2%
+Un usuario ingresa su nombre y gustos musicales por teclado separados por coma, verifica si el 
+usuario ingresó un nombre válido usando truthiness, convertir los gustos musicales en una 
+lista y verifica si tiene el gusto rock en su lista de gustos musicales
+Nombre: Jhon Doe
+Gustos musicales: rock,pop,jazz"""
