37d343d339e313aea7a8bef3530245f5a50712b6
Pomca-laura <pomca.laura@gmail.com>
2024-06-08 10:51:48 -0500

    sesion 15

---
+++ retos_sesion_15/ejercicio_01.py
@@ -0,0 +1,17 @@
+"""finally: Bloque de código que se ejecuta siempre
+raise: Genera una excepción
+pass: No hace nada
+Podemos crear excepciones personalizadas con class
+RETOS
+Crear una carpeta con el nombre "retos_sesion_15" en la cual por cada ejercicio debe crear un script de python
+
+# Ejemplo carpeta 
+psg-example/
+    retos_sesion_15/
+        ejercicio_01.py
+        ejercicio_02.py
+        ejercicio_03.py
+        sesion15.ipynb
+Subir la carpeta a su repositorio en GitHub cuando termine los retos
+
+Crear una calculadora que solicite dos números y realice las operaciones básicas de suma, resta, multiplicación y división con manejo de excepciones, para salir del programa se debe ingresar "salir""""
---
+++ retos_sesion_15/ejercicio_03.py
@@ -0,0 +1,1 @@
+"""Simular un cajero automático que solicite un monto a retirar, si el monto es mayor al saldo lanzar una excepción personalizada y si el monto es mayor a 1000 lanzar una excepción genérica"""
